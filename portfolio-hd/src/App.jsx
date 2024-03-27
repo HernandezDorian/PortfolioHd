7c8e0decf17c721f4029f61a091f481a5964800c
HernandezDorian <143510559+HernandezDorian@users.noreply.github.com>
2024-03-27 11:41:18 +0100

    Calendar

--- portfolio-hd/src/App.jsx
+++ portfolio-hd/src/App.jsx
@@ -1,21 +1,29 @@
 // import "./App.css";
 import "./App.scss";
 import AboutMe from "./components/about-me/about-me.jsx";
+import Calendar from "./components/calendar/calendar.jsx";
+
 import Header from "./components/header/header";
 import HelloMain from "./components/hello/hello.jsx";
-
+// import GitHubCalendar from "react-github-calendar";
 function App() {
   return (
     <>
-      {/* Header */}
+      {/* Header GRADIENT*/}
       <Header />
-      {/* Bonjour jusqu'au typewritter */}
+      {/* Bonjour jusqu'au typewritter R */}
       <HelloMain />
-      {/* A propos de moi */}
+      {/* A propos de moi  GRADIENT*/}
       <AboutMe />
-
-      <section>A propos de moi</section>
+      {/* Languages  R*/}
+      {/* <Calendar /> */}
+      <Calendar />
+      <section>Languages</section>
+      {/* Jours ou je code Gradient */}
+      <section>Jours ou je code</section>
+      {/* Mes projets R */}
       <section>Mes projets</section>
+      {/* Contact Gradient */}
       <section>Contact</section>
     </>
   );
---
+++ portfolio-hd/src/components/calendar/calendar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import GitHubCalendar from "react-github-calendar"; // https://grubersjoe.github.io/react-github-calendar/
+import PageContainer from "../page-container/page-container";
+
+export default function calendar() {
+  return (
+    <PageContainer>
+      <GitHubCalendar username="HernandezDorian" colorScheme="dark" />
+    </PageContainer>
+  );
+}
