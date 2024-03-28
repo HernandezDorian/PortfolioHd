cb002dababcdb4347575899f18c19c0e6867c499
HernandezDorian <143510559+HernandezDorian@users.noreply.github.com>
2024-03-28 21:56:52 +0100

    Footer + Correction Header

--- portfolio-hd/src/components/footer/footer.jsx
+++ portfolio-hd/src/components/footer/footer.jsx
@@ -1,11 +1,45 @@
 import React from "react";
 import MsgSVG from "../../assets/Message_and_nofitications.svg";
+import PageContainer from "../page-container/page-container";
 
 export default function Footer() {
   return (
-    <footer className="Footer">
-      <div className="Footer__contact"></div>
-      <img src={MsgSVG} className="Footer__SVG"></img>
+    <footer className="ft">
+      <PageContainer>
+        <div className="Footer">
+          <h2 className="Footer__title">ME CONTACTER</h2>
+          <div className="Footer--container">
+            <div className="Footer__contact">
+              <form className="Footer__contact__form">
+                <input
+                  className="Footer__contact__form__input"
+                  type="text"
+                  id="name"
+                  name="name"
+                  placeholder="Votre nom"
+                />
+                <input
+                  className="Footer__contact__form__input"
+                  type="email"
+                  id="email"
+                  name="email"
+                  placeholder="Votre email"
+                />
+                <textarea
+                  className="Footer__contact__form__input"
+                  id="message"
+                  name="message"
+                  placeholder="Votre message"
+                ></textarea>
+                <button type="submit" className="button">
+                  Envoyer
+                </button>
+              </form>
+            </div>
+            <img src={MsgSVG} className="Footer__SVG"></img>
+          </div>
+        </div>
+      </PageContainer>
     </footer>
   );
 }
--- portfolio-hd/src/components/header/header.jsx
+++ portfolio-hd/src/components/header/header.jsx
@@ -2,23 +2,26 @@ import PageContainer from "../page-container/page-container";
 
 export default function Header() {
   return (
-    <header className="Header">
+    <header>
       <PageContainer>
-        <div className="Header__right">
-          <h1 className="Header__right__logo">
-            <a href="#hello">Hd.</a>
-          </h1>
-          <nav className="Header__right__nav">
-            <a href="#about" className="Header__right__nav__link">
-              A propos
-            </a>
-            <a href="#" className="Header__right__nav__link">
-              Projets
-            </a>
-            <a href="#" className="Header__right__nav__link">
-              Contact
-            </a>
-          </nav>
+        <div className="Header">
+          <div className="Header__right">
+            <h1 className="Header__right__logo">
+              <a href="#hello">Hd.</a>
+            </h1>
+            <nav className="Header__right__nav">
+              <a href="#about" className="Header__right__nav__link">
+                A propos
+              </a>
+              <a href="#" className="Header__right__nav__link">
+                Projets
+              </a>
+              <a href="#" className="Header__right__nav__link">
+                Contact
+              </a>
+            </nav>
+          </div>
+          <div className="Header__left"></div>
         </div>
       </PageContainer>
     </header>
