e9e22c2b74d0bb4f8e4089af55c5a6d8ad361204
HernandezDorian <143510559+HernandezDorian@users.noreply.github.com>
2024-04-08 20:08:02 +0200

    Phrase description première version

--- src/components/about-me/about-me.jsx
+++ src/components/about-me/about-me.jsx
@@ -17,22 +17,23 @@ export default function AboutMe() {
           <div className="Aboutme__int__txtZone">
             <h2 className="Aboutme__int__txtZone__title">A PROPOS DE MOI</h2>
             <p className="Aboutme__int__txtZone__info">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Donec
-              pretium vulputate sapien nec sagittis aliquam. Adipiscing elit
-              duis tristique sollicitudin nibh. Dignissim enim sit amet
-              venenatis urna cursus. Pellentesque massa placerat duis ultricies
-              lacus sed. Eu consequat ac felis donec et. Cras fermentum odio eu
-              feugiat pretium nibh ipsum consequat. Lobortis scelerisque
-              fermentum dui faucibus in ornare quam. Ipsum nunc aliquet bibendum
-              enim facilisis. In ornare quam viverra orci sagittis eu volutpat.
-              Nulla at volutpat diam ut venenatis tellus. Lobortis mattis
-              aliquam faucibus purus in massa tempor nec. Sit amet massa vitae
-              tortor condimentum lacinia quis vel. Suspendisse interdum
-              consectetur libero id. Sit amet massa vitae tortor condimentum.
-              Enim praesent elementum facilisis leo vel. Amet commodo nulla
-              facilisi nullam vehicula ipsum a arcu cursus. Pharetra diam sit
-              amet nisl suscipit adipiscing bibendum est.
+              {`
+              Bienvenue dans mon portfolio ! Je suis Hernandez Dorian, un
+              développeur web passionné âgé de 25 ans, spécialisé dans le
+              front-end et plus particulièrement dans le framework React.
+              Diplômé de l'école de formation Openclassroom, j'ai acquis les
+              compétences nécessaires pour concevoir des interfaces utilisateur
+              interactives et esthétiques. Mon parcours académique et mes
+              expériences professionnelles m'ont permis de développer une
+              expertise solide dans le domaine du développement web, en
+              utilisant les dernières technologies et les meilleures pratiques
+              de l'industrie. Je suis constamment à la recherche de nouveaux
+              défis pour améliorer mes compétences et élargir mon champ
+              d'expertise. Dans ce portfolio, vous trouverez une sélection de
+              mes projets les plus représentatifs, qui démontrent mon
+              savoir-faire et ma créativité en matière de développement web.
+              N'hésitez pas à me contacter pour discuter de vos projets et de la
+              façon dont je peux vous aider à les réaliser.`}
             </p>
           </div>
         </div>
