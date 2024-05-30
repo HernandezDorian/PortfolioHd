aa68b8ab84c43925276925ad0d77034fab2fc0e2
HernandezDorian <143510559+HernandezDorian@users.noreply.github.com>
2024-05-30 14:45:44 +0200

    Revert "FadeInScroll"
    This reverts commit 7a8bcbb428093652c8885ee422043109661cd5ee.

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Projets from "./components/projets/projets.jsx";
 import { DarkModeContext } from "./components/dark-mode/DarkModeContext.jsx";
 import LightBg from "./assets/low-poly-abstract-background.webp";
 import DarkBg from "./assets/low-poly-abstract-background-Dar.webp";
-import FadeInSection from "./components/FadeInScroll/FadeInSection/FadeInSection";
+
 function App() {
   const [switchDarkMode, setSwitchDarkMode] = useState(false);
 
@@ -24,24 +24,12 @@ function App() {
   return (
     <DarkModeContext.Provider value={[switchDarkMode, setSwitchDarkMode]}>
       <div className={switchDarkMode ? "dark" : "light"}>
-        <FadeInSection>
-          <Header switchDarkMode={switchDarkMode} />
-        </FadeInSection>
-        <FadeInSection>
-          <HelloMain />
-        </FadeInSection>
-        <FadeInSection>
-          <AboutMe />
-        </FadeInSection>
-        <FadeInSection>
-          <Projets />
-        </FadeInSection>
-        <FadeInSection>
-          <DarkMode />
-        </FadeInSection>
-        <FadeInSection>
-          <Footer />
-        </FadeInSection>
+        <Header switchDarkMode={switchDarkMode} />
+        <HelloMain />
+        <AboutMe />
+        <Projets />
+        <DarkMode />
+        <Footer />
       </div>
     </DarkModeContext.Provider>
   );
